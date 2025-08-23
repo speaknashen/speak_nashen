f2b4a60208a36947e53166641444169809a07349
Muhammed jasim k <kmuhammed@wallpostdev.com>
2025-08-23 14:08:55 +0530

    file changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow',
   icons: {
-    icon: "images/logo-small.png",
+    icon: '/favicon.png',
   },
 };
 
